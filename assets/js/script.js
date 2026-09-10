773e7384163c0778300e5258cdb8a454734e19b7
Adi Satria Permana <adisatriap12@gmail.com>
2026-09-10 16:13:03 +0700

    atur index

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,17 @@ const weekButtons = document.querySelectorAll(".week-button");
 
 weekButtons.forEach(function(button) {
 
+
     button.addEventListener("click", function() {
+
 
         const weekItem = this.parentElement;
 
+
         weekItem.classList.toggle("active");
+
 
     });
 
-});
 
+});
