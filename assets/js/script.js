bacbb16bd2161817fd4313a9a39050612e3dd869
Adi Satria Permana <adisatriap12@gmail.com>
2026-09-17 07:14:26 +0700

    update matkul

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,9 @@
 const weekButtons = document.querySelectorAll(".week-button");
 
+weekButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    const weekItem = this.parentElement;
 
-weekButtons.forEach(function(button) {
-
-
-    button.addEventListener("click", function() {
-
-
-        const weekItem = this.parentElement;
-
-
-        weekItem.classList.toggle("active");
-
-
-    });
-
-
+    weekItem.classList.toggle("active");
+  });
 });
